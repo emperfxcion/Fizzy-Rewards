557e1cc34fd171f1d497eaff4cf7af28ba1ceb67
emperfxcion <emperfxcion@gmail.com>
2025-08-27 15:19:20 -0400

    Update canvas-confetti.d.ts

--- types/canvas-confetti.d.ts
+++ types/canvas-confetti.d.ts
@@ -1,5 +1,4 @@
 declare module 'canvas-confetti' {
-  // Minimal typing to keep TS happy
   type ConfettiOptions = Record<string, unknown>;
   function confetti(opts?: ConfettiOptions): void;
   export default confetti;
